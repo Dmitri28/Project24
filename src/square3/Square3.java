d831815a3550ca7874309203a64adb3e10ee4e41
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 13:24:09 +0200

    Project24
    Project24

---
+++ src/square3/Square3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package square3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Square3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner in=new Scanner(System.in);
+     int a=in.nextInt();
+     System.out.println("=");
+     System.out.println(a*2);
+     System.out.println("&");
+     int a1=a;
+     System.out.println(a1*3);
+      
+      
+    }
+    
+}
